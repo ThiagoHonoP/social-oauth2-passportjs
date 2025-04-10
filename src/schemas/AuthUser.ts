34aa521ac2c476e7385a3e981069d6f9e15c51d8
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2025-04-10 01:24:40 -0300

    feat: (auth) create AuthSchema

---
+++ src/schemas/AuthUser.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+
+const AuthSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default model("users", AuthSchema);
